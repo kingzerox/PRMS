6029a898a39c3841282bf1cb945d1134dc8d98f6
kingzerox <kingzerox@hotmail.com>
2019-03-27 06:25:21 +0000

    编辑资料

---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Auth;
+
+class UserRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' . Auth::id(),
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'name.regex' => '用户名只支持英文、数字、横杠和下划线。',
+            'name.between' => '用户名必须介于 3 - 25 个字符之间。',
+            'name.required' => '用户名不能为空。',
+        ];
+    }
+}
