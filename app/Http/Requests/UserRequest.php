b21206fcd3f6a512d13afee5ecd2298d3309cbb8
kingzerox <kingzerox@hotmail.com>
2019-03-27 06:53:53 +0000

    限制头像

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -16,11 +16,14 @@ class UserRequest extends FormRequest
     {
         return [
             'name' => 'required|between:2,25|unique:users,name,' . Auth::id(),
+            'avatar' => 'mimes:jpeg,bmp,png,gif|dimensions:min_width=208,min_height=208',
         ];
     }
     public function messages()
     {
         return [
+            'avatar.mimes' =>'头像必须是 jpeg, bmp, png, gif 格式的图片',
+            'avatar.dimensions' => '图片的清晰度不够，宽和高需要 208px 以上',
             'name.between' => '用户名必须介于 3 - 25 个字符之间。',
             'name.required' => '用户名不能为空。',
         ];
