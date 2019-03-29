6d25e5ed5f51c465d0450201d7c8023f9435abbb
kingzerox <kingzerox@hotmail.com>
2019-03-29 15:25:00 +0000

    公告上传图片

--- routes/web.php
+++ routes/web.php
@@ -40,5 +40,6 @@ Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit'
 Route::get('permission-denied', 'PagesController@permissionDenied')->name('permission-denied');
 //话题
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
-
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
+//上传图片
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
