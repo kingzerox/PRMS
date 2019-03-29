6cede23ca9d41c0607b2fa51f20497e57e03303e
kingzerox <kingzerox@hotmail.com>
2019-03-29 15:41:12 +0000

    话题内容页面

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
   <title>@yield('title', 'PRMS') - 物理资源在线管理系统</title>
+  <meta name="description" content="@yield('description', '物理资源在线管理系统')" />
 
   <!-- Styles -->
   <link href="{{ mix('css/app.css') }}" rel="stylesheet">
