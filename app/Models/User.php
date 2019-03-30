fdea6b7fb2333be8d541c131a94bc264423f5b64
kingzerox <kingzerox@hotmail.com>
2019-03-30 15:37:28 +0000

    回复列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -54,4 +54,9 @@ class User extends Authenticatable implements MustVerifyEmailContract
     {
         return $this->hasMany(Reply::class);
     }
+
+    public function topics()
+    {
+        return $this->hasMany(Topic::class);
+    }
 }
